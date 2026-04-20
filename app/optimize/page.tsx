31f761c474a5a36f9e6551f73c3306f45eea6e94
asilva35 <sistev.contacto@gmail.com>
2026-04-20 14:37:24 -0500

    Optimize Test 04

--- app/optimize/page.tsx
+++ app/optimize/page.tsx
@@ -29,18 +29,25 @@ export const metadata: Metadata = {
 export default function Page() {
     return (
         <>
-            <Head>
-                <Partytown debug={true} forward={["dataLayer.push"]} />
-                <Script type="text/partytown">
-                    {`
+            <Partytown
+                debug={true}
+                forward={["dataLayer.push"]}
+                lib="/~partytown/"
+            />
+            <Script
+                strategy="worker" // Next.js detecta automáticamente Partytown con esto
+                type="text/partytown"
+                id="gtm-script"
+                dangerouslySetInnerHTML={{
+                    __html: `
                         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                         })(window,document,'script','dataLayer','GTM-TR9VGQ5G');
-                    `}
-                </Script>
-            </Head>
+                    `,
+                }}
+            />
             <Suspense fallback={
                 <div className="min-h-screen bg-neutral-950 flex items-center justify-center">
                     <div className="w-8 h-8 rounded-full border-2 border-emerald-500 border-t-transparent animate-spin" />
