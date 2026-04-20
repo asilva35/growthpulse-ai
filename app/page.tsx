dc50766d5e5b1845baa124b561d687faffe19797
asilva35 <sistev.contacto@gmail.com>
2026-04-20 11:40:04 -0500

    Tag Manager in Page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ import {
 } from "lucide-react";
 
 import Head from "next/head";
-import { Partytown } from "@qwik.dev/partytown/react";
+import Script from "next/script";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
@@ -1772,17 +1772,15 @@ export default function Page() {
   return (
     <>
       <Head>
-        <Partytown debug={true} forward={["dataLayer.push"]} />
-        <script
-          type="text/partytown"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-TR9VGQ5G');`,
-          }}
-        />
+          })(window,document,'script','dataLayer','GTM-TR9VGQ5G');
+        `}
+        </Script>
       </Head>
       <Suspense fallback={
         <div className="min-h-screen bg-neutral-950 flex items-center justify-center">
